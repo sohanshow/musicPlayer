89690d322de81247450c508b41dabe89939907fb
sohanshow <sohanshow@gmail.com>
2022-07-08 19:01:05 -0400

    + Added songs and preBtn and nextBtn

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+//Music
+const songs = [
+
+
+{
+    name:'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
+},
+
+{
+    name:'jacinto-2',
+    displayName: 'Seven Nation army (Remix)',
+    artist: 'Jacinto Design',
+},
+
+{
+    name:'jacinto-3',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
+}
+
+];
 
 
 //Check if playing
@@ -34,4 +61,54 @@ pauseSong = () =>{
 // Play or Pause event listener
 
 playBtn.addEventListener('click', () => (isPlaying? pauseSong() : playSong()));
+
+
+//Updateing the DOM
+
+loadSong = (song) =>{
+
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+     
+}
+
+//Current Song
+let songIndex = 0;
+
+//Previous Song
+prevSong = ()=>{
+
+    if (songIndex == 0)
+    {
+        songIndex = songs.length - 1;
+    }
+    else
+    songIndex--;
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+//Next Song
+nextSong = ()=>{
+    if (songIndex == songs.length)
+    songIndex = 0;
+    else
+    songIndex++;
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+
+//Upon Load
+loadSong(songs[songIndex]);
+
+
+//Event Listeners
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
 
